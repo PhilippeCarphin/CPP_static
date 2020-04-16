aca30e7a885e6656c773dce71a578277d52941b7
Philippe Carphin <philippe.carphin@polymtl.ca>
2020-04-16 15:37:47 -0400

    Include the warning

--- mylib.hpp
+++ mylib.hpp
@@ -4,6 +4,12 @@ class MyLib
     public:
     static const float pi;
     float tau;
-    float e = 2.71828; // Note: This produces a warning if we are not using C++11.
+    float e = 2.71828;
+    // Note: This produces a warning if we are not using C++11.
+    // In file included from /Users/pcarphin/Documents/GitHub/CPP_static/main.cpp:4:
+    // /Users/pcarphin/Documents/GitHub/CPP_static/mylib.hpp:7:13: warning: in-class initialization of non-static data member is a C++11 extension
+    //       [-Wc++11-extensions]
+    //     float e = 2.71828; // Note: This produces a warning if we are not using C++11.
+    //             ^
     MyLib();
 };
